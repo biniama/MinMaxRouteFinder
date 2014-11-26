2ce6acd98efb525b58ae6f9f253d9b82a5f8b0ac
basnake <basnake@apposit.com>
2014-11-26 18:17:26 +0300

    Initial commit

---
+++ MinMaxRouteFinder/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRouteFinder
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.txtDestinationPlace = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtFileSelected = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(270, 156)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(47, 23)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "&Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'txtDestinationPlace
+        '
+        Me.txtDestinationPlace.Location = New System.Drawing.Point(37, 156)
+        Me.txtDestinationPlace.Name = "txtDestinationPlace"
+        Me.txtDestinationPlace.Size = New System.Drawing.Size(199, 20)
+        Me.txtDestinationPlace.TabIndex = 1
+        Me.txtDestinationPlace.Text = "Saris"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 117)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(427, 23)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Please enter the destination place you want to go to:"
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.BackColor = System.Drawing.SystemColors.Highlight
+        Me.btnBrowse.Location = New System.Drawing.Point(474, 69)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(34, 23)
+        Me.btnBrowse.TabIndex = 22
+        Me.btnBrowse.Text = "..."
+        Me.btnBrowse.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(31, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(316, 24)
+        Me.Label2.TabIndex = 24
+        Me.Label2.Text = "Please Select the location of the file:"
+        '
+        'txtFileSelected
+        '
+        Me.txtFileSelected.Location = New System.Drawing.Point(36, 72)
+        Me.txtFileSelected.Name = "txtFileSelected"
+        Me.txtFileSelected.ReadOnly = True
+        Me.txtFileSelected.Size = New System.Drawing.Size(407, 20)
+        Me.txtFileSelected.TabIndex = 23
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'frmRouteFinder
+        '
+        Me.AcceptButton = Me.btnGo
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(533, 209)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtFileSelected)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDestinationPlace)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "frmRouteFinder"
+        Me.Text = "Route Finder Using Min-Max Algorithm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents txtDestinationPlace As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtFileSelected As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ MinMaxRouteFinder/Form1.vb
@@ -0,0 +1,461 @@
+﻿Imports System.IO
+
+Public Class frmRouteFinder
+
+    Dim fileName As String = "C:\Routes2.txt"
+
+    'Declare a StreamReader object and read from the file
+    Private myStreamReader As StreamReader
+
+    'Declare an object to store the whole content of the file and 
+    'Save the whole content of the file in the variable called "content"
+    Private item As Object
+
+    'Declare a string variable to store the content that is read from the file
+    Private content As String = String.Empty
+
+    Public Structure strOurNodes
+
+        Dim node As String
+        Dim successors As List(Of String)
+        Dim minmaxLevel As String
+        Dim distance As Integer
+
+    End Structure
+
+    Public objOurNodes As New List(Of strOurNodes)
+
+    Public ourNode As strOurNodes
+
+    Public ourDictionary As New Dictionary(Of String, String)
+
+    Public Sub ourDictionaryInitializer()
+
+        For Each n In objOurNodes
+
+            If n.successors.Count = 0 Then
+                ourDictionary.Add(n.node, n.distance)
+            End If
+
+        Next
+
+        'With ourDictionary
+
+        '    .Add("Bulgaria", 7)
+        '    .Add("Sarbet", 8)
+        '    .Add("Stadium", 9)
+        '    .Add("Cherkos", 7)
+        '    .Add("Dembel", 13)
+        '    .Add("Kera2", 9)
+        '    .Add("ShewaDabo", 14)
+        '    .Add("Mekanisa", 15)
+
+        'End With
+
+    End Sub
+
+    Dim childValue As New Dictionary(Of String, Integer)
+
+    Public Function MinMax(ByVal newNode As String, ByVal depth As Integer) As Integer
+
+        Dim minimumValue As Integer = 1000000000
+        Dim maximumValue As Integer = -1000000000
+        Dim depthValue As Integer = 0
+
+        Dim maxDepth As Integer = 3
+
+        'childValue = New Dictionary(Of String, Integer)
+
+        For Each n In objOurNodes
+
+            If n.node.Equals(newNode) Then
+
+                ' Checks if the node is the root node or depth 
+                If depth = maxDepth Then
+
+                    depthValue = ourDictionary.Item(n.node)
+                    childValue.Add(newNode, depthValue)
+
+                    Return depthValue
+
+                ElseIf n.minmaxLevel.Equals("min") Then
+
+                    'value = 1000000000
+
+                    For Each child In n.successors
+
+                        minimumValue = Math.Min(minimumValue, MinMax(child, depth + 1))
+                        'minimumValue = MinMax(child, depth + 1)
+                        If childValue.ContainsKey(newNode) = False Then
+                            childValue.Add(newNode, minimumValue)
+
+                        End If
+
+                    Next
+
+                    Return minimumValue
+
+                ElseIf n.minmaxLevel.Equals("max") Then
+
+                    '   value = -1000000000
+
+                    For Each child In n.successors
+
+                        maximumValue = Math.Max(maximumValue, MinMax(child, depth + 1))
+                        'maximumValue = MinMax(child, depth + 1)
+                        If childValue.ContainsKey(newNode) = False Then
+                            childValue.Add(newNode, maximumValue)
+                            'ElseIf childValue.ContainsKey(n.node) = False Then
+                            '    childValue.Add(n.node, maximumValue)
+                            'maximumValue = -1000000000
+                        End If
+
+                    Next
+
+                    Return maximumValue
+
+                End If
+
+            End If
+
+        Next
+
+    End Function
+
+    Dim optimalPath As New List(Of String)
+
+    Private Sub btnGo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGo.Click
+
+        ''
+        fileName = "C:\ourRoutes.txt"
+        ''
+
+        If IsNothing(fileName) Or fileName.Equals(String.Empty) = True Then
+            MessageBox.Show("Please select the location of the file.", "Route Finding Using Min-Max Algorithm", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf fileName.Equals(String.Empty) = False Then
+            content = ReadFromFile(fileName)
+            If content.Equals("No") Then
+                MessageBox.Show("Sorry! The file you selected is not found or cannot be read.", "Route Finding Using Min-Max Algorithm", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+            ourTokenizer(content)
+
+            Dim strDestination As String
+            strDestination = txtDestinationPlace.Text
+
+            ''
+            strDestination = "Saris"
+            ''
+
+            Dim ourList As New List(Of String)
+
+            ' Initialize the dictionary
+            If IsNothing(ourDictionary) Or ourDictionary.Count = 0 Then
+                ourDictionaryInitializer()
+            End If
+
+            childValue.Clear()
+
+            Dim finalValue As Integer = 0
+            finalValue = MinMax(strDestination, 0)
+
+            MessageBox.Show("The optimal kilometer is: " & finalValue)
+
+            For Each x In childValue
+
+                If x.Value = finalValue Then
+
+                    ourList.Add(x.Key)
+
+                End If
+
+            Next
+
+            Dim toBePrinted As String = String.Empty
+
+            For Each o In ourList
+
+                toBePrinted &= o & " --> "
+
+            Next
+
+            MessageBox.Show("The optimal path is : " & toBePrinted & strDestination)
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        '   Call ourInitializer()
+        '  Call ourDictionaryInitializer()
+
+        txtDestinationPlace.Text = String.Empty
+        txtDestinationPlace.Focus()
+
+
+
+
+        '  Dim finalPlace As String
+
+        ' finalPlace = ourDictionary(finalValue)
+        'ourDictionary.
+        ' optimalPath.Add(finalPlace)
+
+        'For Each place In objOurNodes
+
+        '    For Each s In place.successors
+        '        If s.Equals(optimalPath.Last) Then
+
+        '            optimalPath.Add(place.node)
+        '            Exit For
+        '        End If
+
+        '    Next
+
+        'Next
+
+        ''   optimalPath.Add()
+
+        'For Each x In optimalPath
+        '    MessageBox.Show(x)
+        'Next
+
+
+    End Sub
+
+    Private Sub btnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBrowse.Click
+
+        OpenFileDialog1.FileName = "C:\Routes3.txt"
+
+        'Show the open dialog and if the user clicks the open button load the file
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Try
+                'Save the file name in the variable called "filename1"
+                fileName = OpenFileDialog1.FileName
+                txtFileSelected.Text = fileName
+
+            Catch ex As Exception               'FileNotFoundException
+                'If there is any exception related to opening file, display an exception (error) message to the user
+                MessageBox.Show("The file you selected is not found.", "Route Finding Using Min-Max Algorithm", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+
+
+    End Sub
+
+    Public Function ReadFromFile(ByVal filename As String) As String
+
+        If IO.File.Exists(filename) Then
+            myStreamReader = IO.File.OpenText(filename)
+
+            content = String.Empty
+
+            item = myStreamReader.ReadToEnd
+
+            content = content & item.ToString.Trim
+
+            'When finished reading from the file, close the StreamReader
+            myStreamReader.Close()
+
+            Return content
+
+        Else
+            Return "No"
+        End If
+
+    End Function
+
+    Private Sub ourTokenizer(ByVal content As String)
+
+        Dim separators As Char()
+        separators = {"{", "_", "}", "~", ",", "."}
+
+        Dim mylist As New List(Of String)
+
+        mylist = content.Split(separators).ToList()
+
+        Dim successorsLocal As New List(Of String)
+
+        For e As Integer = 0 To mylist.Count - 1 Step 1
+
+            successorsLocal.Clear()
+
+            If mylist(e).Trim.Equals(String.Empty) = False Then
+
+                ourNode.node = mylist(e).Trim
+
+                If mylist(e + 1).Trim.Equals("*") Then
+                    ourNode.node = mylist(e).Trim
+
+                    '  ourNode.successors.Add(mylist(e + 2))
+                    e += 1
+                    While IsNumeric(mylist(e + 1)) = False
+                        e += 1
+                        successorsLocal.Add(mylist(e))
+
+                    End While
+
+                    e += 1
+                    If IsNumeric(mylist(e)) Then
+                        If mylist(e) = 1 Then
+                            ourNode.minmaxLevel = "max"
+                        ElseIf mylist(e) = 0 Then
+                            ourNode.minmaxLevel = "min"
+                        Else
+                            ourNode.distance = mylist(e)
+                        End If
+                    End If
+
+                End If
+
+                ourNode.successors = Nothing
+
+                'If IsNothing(ourNode.successors) = False Then
+                '    ourNode.successors.RemoveRange(0, ourNode.successors.Count)
+                'End If
+                ' For Each s In successorsLocal
+                ourNode.successors = New List(Of String)(successorsLocal)
+                'Next
+                objOurNodes.Add(ourNode)
+
+            End If
+
+        Next
+
+    End Sub
+
+    Public Sub ourInitializer()
+        ourNode.successors = New List(Of String)
+
+        ourNode.node = "Saris"
+        ourNode.successors.Add("Gotera")
+        ourNode.successors.Add("BihereTsige")
+        ourNode.successors.Add("Abo")
+        ourNode.minmaxLevel = "max"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Gotera"
+        ourNode.successors.Add("Kera")
+        ourNode.successors.Add("Lancha")
+        ourNode.successors.Add("WolloSefer")
+        ourNode.minmaxLevel = "min"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Kera"
+        ourNode.successors.Add("Bulgaria")
+        ourNode.successors.Add("Sarbet")
+        ourNode.minmaxLevel = "max"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Lancha"
+        ourNode.successors.Add("Stadium")
+        ourNode.successors.Add("Cherkos")
+        ourNode.minmaxLevel = "max"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "WolloSefer"
+        ourNode.successors.Add("Dembel")
+        ourNode.minmaxLevel = "max"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Bulgaria"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Sarbet"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Stadium"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Cherkos"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Dembel"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "BihereTsige"
+        ourNode.successors.Add("MebratHail")
+        ourNode.minmaxLevel = "min"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "MebratHail"
+        ourNode.successors.Add("Kera2")
+        ourNode.minmaxLevel = "max"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Kera2"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Abo"
+        ourNode.successors.Add("Bole")
+        ourNode.successors.Add("KalityMaseltegna")
+        ourNode.minmaxLevel = "min"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Bole"
+        ourNode.successors.Add("ShewaDabo")
+        ourNode.minmaxLevel = "max"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "KalityMaseltegna"
+        ourNode.successors.Add("Mekanisa")
+        ourNode.minmaxLevel = "max"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "ShewaDabo"
+        objOurNodes.Add(ourNode)
+
+        ResetNodes()
+
+        ourNode.node = "Mekanisa"
+        objOurNodes.Add(ourNode)
+
+    End Sub
+
+    Public Sub ResetNodes()
+
+        ourNode.node = Nothing
+        ourNode.successors = New List(Of String)
+
+    End Sub
+
+    Public Function goalTestFunction() As Integer
+
+        'If 
+
+    End Function
+
+End Class
